22b458a0b38db9e68c12c1787319d41fbdd7dd32
jordanrob709 <jordanrob709@gmail.com>
2020-06-30 13:40:56 +0300

    added database schema and endpoint for signup

---
+++ models/tweeps.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const tweepSchema = new mongoose.Schema({
+  userName: {
+    type: String,
+    required: true,
+    min: 5,
+    max: 255,
+  },
+  email: {
+    type: String,
+    required: true,
+    min: 5,
+    max: 255,
+  },
+  password: {
+    type: String,
+    required: true,
+    min: 8,
+    max: 2000,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
