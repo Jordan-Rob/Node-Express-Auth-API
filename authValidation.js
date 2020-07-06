6065d572a8f4ecab6c638d74dc78fb1ad0c30efa
jordanrob709 <jordanrob709@gmail.com>
2020-07-06 13:19:35 +0300

    added login validation

--- authValidation.js
+++ authValidation.js
@@ -9,4 +9,13 @@ const signupChecks = (data) => {
   return validationSchema.validate(data);
 };
 
+const loginChecks = (data) => {
+  const validationSchema = joi.object({
+    email: joi.string().min(4).required().email(),
+    password: joi.string().min(4).required()
+  })
+  return validationSchema.validate(data)
+}
+
 module.exports.signupChecks = signupChecks;
+module.exports.loginChecks = loginChecks
